6c64588e605f8d2ceab93fc4da51eea5b4aa22c5
jparkin-Stanford <66646707+jparkin-Stanford@users.noreply.github.com>
2022-03-03 13:28:01 -0800

    Add files via upload
    Updated Log Model code and necessary code for best L1 model

--- Log_Model.R
+++ Log_Model.R
@@ -6,14 +6,13 @@ library(quantmod)
 library(tidyverse)
 library(data.table)
 library("xlsx")
-library("dplyr")
-library("Matrix")
 library(fastDummies)
 library(leaps)
 library(tidyverse)
 library(ROCR)
 library(glmnet)
 library(fitdistrplus)
+library(QRM)
 
 ############
 #Data Import
@@ -46,6 +45,10 @@ auc = unlist(slot(performance(log_model_train_prediction, 'auc'), 'y.values'))
 auc
 #0.7541353
 
+#Variable selection
+qplot(y= log_model$coefficients) + labs(title = "Basic Logistic Model Coeffcients", x= "Covariates", y= "Coeffcient Value")
+log_model_coefficients = as.data.frame(log_model$coefficients)
+
 #Test ROC AUC
 log_model_test_prediction = predict(log_model, newdata = test_data, type="response")
 log_model_test_prediction = prediction(log_model_test_prediction, test_data$Default)
@@ -56,9 +59,10 @@ auc
 #Plotting ROCs
 roc_train = performance(log_model_train_prediction,"tpr","fpr")
 roc_test = performance(log_model_test_prediction,"tpr","fpr")
-plot(roc_train, col = 'red', main = 'Basic Logistic Model Training ROC (red) vs. Testing ROC (blue)')
+plot(roc_train, col = 'red', main = 'Basic Logistic Model Training ROC vs. Testing ROC')
 plot(roc_test, add = TRUE, col = 'blue')
 abline(a = 0, b = 1) 
+legend(x = "bottomright", legend = c("Training ROC", "Testing ROC"), lty = c(1, 1), col = c("red", "blue"), lwd = 1) 
 
 ################################
 #Ridge and Lasso Logisitic Model
@@ -94,14 +98,16 @@ for(i in 1:10){
 best_L1_lambda_index = which.max(AUC_L1_validation)
 best_L1_lambda = model_L1$lambda[best_L1_lambda_index]
 best_L1_AUC = max(AUC_L1_validation)
-best_L1_AUC
+L1_summary = as.data.frame(model_L1$lambda)
+L1_summary$AUC = AUC_L1_validation
 #0.6679918
 
 #Best L2 Hyperparameter
 best_L2_lambda_index = which.max(AUC_L2_validation)
 best_L2_lambda = model_L2$lambda[best_L2_lambda_index]
 best_L2_AUC = max(AUC_L2_validation)
-best_L2_AUC
+L2_summary = as.data.frame(model_L2$lambda)
+L2_summary$AUC = AUC_L2_validation
 #0.6633677
 
 #Testing: best L1  model
@@ -116,15 +122,17 @@ auc
 L1_coef = as.matrix(coef(model_L1, s=model_L1$lambda[best_L1_lambda_index]))
 rownames(L1_coef) = rownames(coef(model_L1))
 qplot(y= L1_coef[,1]) + labs(title = "Best L1 Logistic Model Coeffcients", x= "Covariates", y= "Coeffcient Value")
+L1_coef = as.data.frame(L1_coef)
 
 #Plotting ROCs
 roc_train = performance(prediction(prediction_L1_train[,best_L1_lambda_index], train_data$Default),"tpr","fpr")
 roc_validation = performance(prediction(prediction_L1_validation[,best_L1_lambda_index], validation_data$Default),"tpr","fpr")
 roc_test = performance(prediction_test,"tpr","fpr")
-plot(roc_train, col = 'red', main = 'Best L1 Logistic Model Training ROC (red) vs. Validation ROC (green) vs. Testing ROC (blue)')
+plot(roc_train, col = 'red', main = 'Best L1 Logistic Model Training ROC vs. Validation ROC vs. Testing ROC')
 plot(roc_validation, add = TRUE, col = 'green')
 plot(roc_test, add = TRUE, col = 'blue')
 abline(a = 0, b = 1) 
+legend(x = "bottomright", legend = c("Training ROC", "Validation ROC", "Testing ROC"), lty = c(1, 1, 1), col = c("red", "green", "blue"), lwd = 1) 
 
 ################
 #Loss at Default
@@ -151,7 +159,7 @@ for (i in 1:N){
 
 lossDist_norm = fitdist(lossDist, "norm")
 numBins = round((max(lossDist) - min(lossDist))/(2*IQR(lossDist)/(length(lossDist)^(1/3))))
-hist(lossDist, breaks =  numBins, main = "Histogram of Inverse Sampling Loan Loss with Sample Loss Proportion at Default with Fitted Normal Distributioon", xlab="Portfolio Loss", ylab="Train Count") + plot(lossDist_norm)
+hist(lossDist, breaks =  numBins, main = "Histogram of Inverse Sampling Loan Loss with Sample Loss Proportion at Default with Fitted Normal Distribution", xlab="Portfolio Loss", ylab="Train Count") + plot(lossDist_norm)
 
 lossDist_log = log(-lossDist)
 lossDist_log_norm = fitdist(lossDist_log, "norm")
@@ -167,14 +175,19 @@ VaR_95
 VaR_99 = -quantile(lossDist, prob = c(0.01))
 VaR_99
 #9107167225
+Avg_VaR_95 = -sum(lossDist[lossDist < -VaR_95])/ length(lossDist[lossDist < -VaR_95])
+Avg_VaR_95
+#7429254131
+Avg_VaR_99 = -sum(lossDist[lossDist <= -VaR_99])/ length(lossDist[lossDist <= -VaR_99])
+Avg_VaR_99
+#9178419284
 
-dev.new(20, 8)
-hist(lossDist, breaks =  numBins, main = "Histogram of Inverse Sampling Loan Loss with Sample Loss Proportion at Default with Non Parametric VaR", xlab="Portfolio Loss", ylab="Train Count") 
+hist(lossDist, breaks =  numBins, main = "Histogram of Inverse Sampling of Loan Loss with Sample Loss Proportion at Default with Non Parametric VaR", xlab="Portfolio Loss", ylab="Train Count", cex.main=1) 
 abline(v = -VaR_95, col="blue", lwd = 3) 
 abline(v = -VaR_99, col="red", lwd = 3)
-#abline(v = -Avg_VaR_95, col="blue", lty = 2, lwd = 3) 
-#abline(v = -Avg_VaR_99, col="red", lty = 2, lwd = 3)
-legend(x = "topright",legend = c("VaR 95% Level", "VaR 99% Level", "Avg VaR 95% Level", "Avg VaR 99% Level"), lty = c(1, 1, 2, 2), col = c("blue", "red", "blue", "red"), lwd = 3) 
+abline(v = -Avg_VaR_95, col="blue", lty = 2, lwd = 3) 
+abline(v = -Avg_VaR_99, col="red", lty = 2, lwd = 3)
+legend(x = "bottomleft",legend = c("VaR 95% Level", "VaR 99% Level", "Avg VaR 95% Level", "Avg VaR 99% Level"), lty = c(1, 1, 2, 2), col = c("blue", "red", "blue", "red"), lwd = 3)
 
 #Parametric LogNormal Losses VaR based on log model
 VaR_95 = exp(qnorm(0.95, mean = lossDist_log_norm$estimate[1], sd = lossDist_log_norm$estimate[2], lower.tail = TRUE, log.p = FALSE))
@@ -182,13 +195,24 @@ VaR_95
 #3946421326
 VaR_99 = exp(qnorm(0.99, mean = lossDist_log_norm$estimate[1], sd = lossDist_log_norm$estimate[2], lower.tail = TRUE, log.p = FALSE))
 VaR_99
-#29369942288
+#4801390385
 
-dev.new(20, 8)
-denscomp(ft = lossDist_log_norm, legendtext = "Normal",  main = "Histogram of Inverse Sampling Log Loan Loss with Sample Loss Proportion at Default", xlab="Portfolio Log Loss", ylab="Train Count", cex.main=1) 
+x = seq(0.95, 0.999, 0.001)
+Avg_VaR_95 = sum(exp(qnorm(x, mean = lossDist_log_norm$estimate[1], sd = lossDist_log_norm$estimate[2], lower.tail = TRUE, log.p = FALSE)))/length(x)
+Avg_VaR_95
+#31164969343
+
+x = seq(0.99, 0.999, 0.001)
+Avg_VaR_99 = sum(exp(qnorm(x, mean = lossDist_log_norm$estimate[1], sd = lossDist_log_norm$estimate[2], lower.tail = TRUE, log.p = FALSE)))/length(x)
+Avg_VaR_99
+#107438865727
+
+denscomp(ft = lossDist_log_norm, legendtext = "Normal",  main = "Histogram of Inverse Sampling Log Loan Loss with Sample Loss Proportion at Default", xlab="Portfolio Log Loss", ylab="Train Count", xlim = c(10,28))
 abline(v = log(VaR_95), col="blue", lwd = 3) 
 abline(v = log(VaR_99), col="red", lwd = 3)
-#abline(v = log(Avg_VaR_95), col="blue", lty = 2, lwd = 3) 
-#abline(v = log(Avg_VaR_99), col="red", lty = 2, lwd = 3)
-legend(x = "topright", legend = c("VaR 95% Level", "VaR 99% Level", "Avg VaR 95% Level", "Avg VaR 99% Level"), lty = c(1, 1, 2, 2), col = c("blue", "red", "blue", "red"), lwd = 3) 
+abline(v = log(Avg_VaR_95), col="blue", lty = 2, lwd = 3) 
+abline(v = log(Avg_VaR_99), col="red", lty = 2, lwd = 3)
+legend(x = "bottomleft", legend = c("VaR 95% Level", "VaR 99% Level", "Avg VaR 95% Level", "Avg VaR 99% Level"), lty = c(1, 1, 2, 2), col = c("blue", "red", "blue", "red"), lwd = 3) 
+
+
 
